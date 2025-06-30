3803c3270fecea4e488539905a7f61a2c1ff09aa
Rosela Allmeta <roselaallmeta>
2025-06-30 21:00:09 +0200

    Database

---
+++ db/database.py
@@ -0,0 +1,15 @@
+import asyncpg
+
+DATABASE_URL = "postgresql://postgresql://admin:rosi123@localhost:5433/main"
+
+class Postgres:
+    def __init__(self, database_url: str):
+        self.database_url = database_url
+
+    async def connect(self):
+        self.pool = await asyncpg.create_pool(self.database_url)
+
+    async def disconnect(self):
+        self.pool.close()
+
+database = Postgres(DATABASE_URL)
